f1eead08e51d1110a4c83f004349ab5947331cbb
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-06-26 23:42:56 +0900

    vue router のインストール

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import '@/base.css'
-import router from '@/router'
 import App from '@/App.vue'
 
 import '@fontsource-variable/noto-sans-jp/index.css' // 'Noto Sans JP Variable'
@@ -8,6 +7,8 @@ import '@fontsource-variable/noto-sans-jp/index.css' // 'Noto Sans JP Variable'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App)
 
+import router from '@/router'
 app.use(router)
+
 console.log('Mode:', import.meta.env.MODE)
 app.mount('#app')
