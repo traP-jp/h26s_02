c6dd64f7b89a01785a3c77ee379bd4791782f450
hachimitsu-pichi <hachimitsu.38@icloud.com>
2026-06-27 13:00:27 +0900

    Echoのバージョンをv4からv5に更新し、ユーザー名を取得する関数を追加

--- server/handler/auth.go
+++ server/handler/auth.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v5"
 )
 
 // AuthMiddleware : ヘッダーからユーザー名を取得し、Contextにセットするミドルウェア
 func AuthMiddleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
+		return func(c *echo.Context) error {
 			var userName string
 
 			// 1. 環境変数 LOCAL が "true" の場合はローカル環境とみなし固定値をセット
@@ -26,7 +26,7 @@ func AuthMiddleware() echo.MiddlewareFunc {
 
 			// 3. ユーザー名が取得できなかった場合は 401 エラーを返す
 			if userName == "" {
-				return echo.NewHTTPError(http.StatusUnauthorized)
+				return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized: User header missing")
 			}
 
 			// 4. EchoのContextに "userName" というキーでセット
@@ -35,4 +35,18 @@ func AuthMiddleware() echo.MiddlewareFunc {
 			return next(c)
 		}
 	}
+}
+
+// GetUserName: ユーザー名を取得する関数
+func GetUserName(c *echo.Context) (string, error) {
+	if c == nil {
+		return "", echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized: User header missing")
+	}
+
+	val := (*c).Get("userName")
+	userName, ok := val.(string)
+	if !ok {
+		return "", echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized: User header missing")
+	}
+	return userName, nil
 }
