76667ffb1f0bc7864eb55f10b5eb6a684b3ac072
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-06-27 09:46:49 +0900

    タイムラインっぽいもの

--- client/src/components/PostImage.vue
+++ client/src/components/PostImage.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  num: number
+}>()
+</script>
+
 <template>
-  <img src="/images/image 1.png" />
+  <div class="image-wrapper">
+    <img :src="`/images/image ${num}.png`" class="post-image" />
+  </div>
 </template>
+
+<style module>
+.image-wrapper {
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  overflow: hidden;
+  background-color: #f0f0f0;
+}
+
+.post-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+}
+</style>
--- client/src/pages/timeline/TimelineView.vue
+++ client/src/pages/timeline/TimelineView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import PostImage from '@/components/PostImage.vue'
+const posts = Array.from({ length: 10 }, (_, index) => index + 1)
 </script>
 
 <template>
-  <PostImage />
+  <div class="timeline">
+    <PostImage v-for="num in posts" :key="num" :num="num" />
+  </div>
 </template>
+
+<style scoped>
+.timeline {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  gap: 1px;
+  padding: 1px;
+}
+</style>
