6d78583adc64775b478e2ebe1afb015561ade0e6
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-06-27 18:30:02 +0900

    ルートからタグやユーザーの情報を得る

--- client/src/pages/grid/UserView.vue
+++ client/src/pages/grid/UserView.vue
@@ -3,12 +3,16 @@ import PostGrid from '@/pages/grid/PostGrid.vue'
 import GridHeader from '@/components/GridHeader.vue'
 import BottomNav from '@/components/BottomNav.vue'
 import UserIcon from '@/components/UserIcon.vue'
+
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const userId = route.params['userId'] as string
 </script>
 
 <template>
   <PostGrid />
-  <GridHeader :posts="12" :reactions="30">
-    <UserIcon user-id="kitsne" />
+  <GridHeader :posts="12" :reactions="30" :title="userId">
+    <UserIcon :user-id="userId" />
   </GridHeader>
   <BottomNav />
 </template>
