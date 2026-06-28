058197dda8a039c80099f34e76dba8e1f0fb65e9
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-06-28 12:24:07 +0900

    gridに投稿をあてる

--- client/src/pages/grid/UserView.vue
+++ client/src/pages/grid/UserView.vue
@@ -3,14 +3,25 @@ import PostGrid from '@/pages/grid/PostGrid.vue'
 import GridHeader from '@/components/GridHeader.vue'
 import BottomNav from '@/components/BottomNav.vue'
 import UserIcon from '@/components/UserIcon.vue'
+import { api } from '@/schema'
 
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const userId = route.params['userId'] as string
+
+const loadNew = async (existingIds: Set<string>) => {
+  const latestPosts = await api.getPosts(undefined, 20)
+  const newPosts = latestPosts.filter((post) => !existingIds.has(post.id))
+  return newPosts
+}
+
+const loadOld = async (lastId?: string) => {
+  return await api.getPosts(lastId, 20)
+}
 </script>
 
 <template>
-  <PostGrid />
+  <PostGrid :load-new="loadNew" :load-old="loadOld" />
   <GridHeader :posts="12" :reactions="30" :title="userId">
     <UserIcon :user-id="userId" />
   </GridHeader>
